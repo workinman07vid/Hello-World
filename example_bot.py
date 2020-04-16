6130348a58337920689513e91e9fcf653b29bd88
workinman07vid <63797003+workinman07vid@users.noreply.github.com>
2020-04-16 08:36:09 -0600

    Create example_bot.py

---
+++ example_bot.py
@@ -0,0 +1,12 @@
+import discord
+client = discord.Client()
+@client.event
+async def on_ready():
+print('we have logged in as {0.user}'.format(client))
+@client.event
+async def on_message(message):
+if message.author == clinet.user:
+return
+if message.content.startswith($hello'):
+await message channel.send(hello!')
+client.run($ python3 Example_bot.py
